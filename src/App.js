5400b0144d6891023627353da15b9510fd05656e
tanluc <tanluc1200@users.noreply.github.com>
2023-06-04 16:34:06 +0700

    update commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.scss";
-
+import Header from "./components/Header";
+import TableUsers from "./components/TableUsers";
+// import Container from "react-bootstrap/Container";
 function App() {
-    return <div>Hello</div>;
+    return (
+        <div className="app-container">
+            {/* <Container> */}
+                <Header />
+                <TableUsers />
+            {/* </Container> */}
+        </div>
+    );
 }
 
 export default App;
